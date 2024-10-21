5092842fcf6a8a3f580a9ad0eb7f0a85995da1b6
Shakhboz Shodiev <shakhboz.shodiev2001@gmail.com>
2024-10-21 13:35:18 +0200

    my stack

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="flex justify-center items-center">
     <NFlex justify="center" align="center">
       <h1 class="header-title text-2xl m-5">Welcome to my Image Generator, Editor & Variant Creator</h1>
     </NFlex>
